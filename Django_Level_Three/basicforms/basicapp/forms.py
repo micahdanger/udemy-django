283ca7ead75590615e89f595e6894854cab912d1
Micah Beckman <micahthedangerous@protonmail.com>
2019-10-30 17:30:43 -0700

    Finished DL3 - forms validation.

---
+++ Django_Level_Three/basicforms/basicapp/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.core import validators
+
+# putting 'value' as a function input tells django to use this as a validator
+"""
+def check_for_z(value):
+    if value[0].lower() != 'z':
+        raise forms.ValidationError("Name needs to start with z")
+"""
+
+class FormName(forms.Form):
+    """
+    # validator defined above
+    name = forms.CharField(validators=[check_for_z])
+    """
+    name = forms.CharField()
+    email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your email again.')
+    text = forms.CharField(widget=forms.Textarea)
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("MAKE SURE EMAILS MATCH!")
+
+    """
+    botcatcher = forms.CharField(required=False,
+                                widget=forms.HiddenInput,
+                                validators=[validators.MaxLengthValidator(0)])
+    """
+
+
+"""
+    def clean_botcatcher(self):
+        botcatcher = self.cleaned_data['botcatcher']
+
+        if len(botcatcher) > 0:
+            raise forms.ValidationError("Gotcha bot!")
+        return botcatcher
+"""
