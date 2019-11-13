1e2d118a4aaa9ded916231872178bcba4fc9e4d6
Micah Beckman <micahthedangerous@protonmail.com>
2019-11-13 12:24:32 -0800

    Started level 5 project.

---
+++ Django_Level_Five/django_dl5/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/micahbeckman/codeprojects/udemy-django/Django_Level_Five/django_dl5/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
